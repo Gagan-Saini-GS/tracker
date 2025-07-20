3ce54259821c321a0a530e638a32eb58d7062982
Gagan-Saini-GS <gagansaini10999@gmail.com>
2025-07-20 19:39:09 +0530

    Extracted chart data model and its provider

--- lib/screens/statistics/graph_screen.dart
+++ lib/screens/statistics/graph_screen.dart
@@ -1,74 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:tracker/enums/timefilter.dart';
+import 'package:tracker/models/chart_data.dart';
+import 'package:tracker/providers/chart_data_provider.dart';
 import 'package:tracker/providers/expense_type_provider.dart';
 import 'package:tracker/providers/time_filter_provider.dart';
 
-// --- 1. Data Model ---
-class ChartData {
-  ChartData(this.x, this.y, [this.pointColor]);
-  final String x;
-  final double y;
-  final Color? pointColor;
-}
-
-// --- 2. Providers ---
-
-// Provider for chart data (simulated data based on time filter)
-final chartDataProvider = Provider.family<List<ChartData>, TimeFilter>((
-  ref,
-  filter,
-) {
-  // In a real application, you would fetch data based on the filter
-  // For demonstration, we'll generate dummy data.
-  switch (filter) {
-    case TimeFilter.day:
-      return [
-        ChartData('8 AM', 100, const Color(0xFF63B5AF)),
-        ChartData('12 PM', 250),
-        ChartData('4 PM', 150),
-        ChartData('8 PM', 300),
-      ];
-    case TimeFilter.week:
-      return [
-        ChartData('Mon', 120),
-        ChartData('Tue', 280, const Color(0xFF63B5AF)),
-        ChartData('Wed', 190),
-        ChartData('Thu', 350),
-        ChartData('Fri', 220),
-        ChartData('Sat', 400),
-        ChartData('Sun', 300),
-      ];
-    case TimeFilter.month:
-      return [
-        ChartData('Mar', 1230),
-        ChartData('Apr', 850),
-        ChartData('May', 1900, const Color(0xFF63B5AF)),
-        ChartData('Jun', 1500),
-        ChartData('Jul', 1700),
-        ChartData('Aug', 1300),
-        ChartData('Sep', 1600),
-      ];
-    case TimeFilter.year:
-      return [
-        ChartData('Jan', 5000),
-        ChartData('Feb', 6500),
-        ChartData('Mar', 5800, const Color(0xFF63B5AF)),
-        ChartData('Apr', 7200),
-        ChartData('May', 6000),
-        ChartData('Jun', 7500),
-        ChartData('Jul', 6800),
-        ChartData('Aug', 8000),
-        ChartData('Sep', 7100),
-        ChartData('Oct', 8500),
-        ChartData('Nov', 7800),
-        ChartData('Dec', 9000),
-      ];
-  }
-});
-
-// --- 3. Chart Widget ---
 class ReusableLineChart extends ConsumerWidget {
   const ReusableLineChart({super.key});
 
