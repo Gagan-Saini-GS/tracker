0ac6d87bd1b94919da012e9d84c083edc434ab50
Gagan-Saini-GS <gagansaini10999@gmail.com>
2025-07-27 12:31:11 +0530

    Removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tracker/utils/constants.dart';
 import 'routes/app_router.dart';
 
 void main() {
